1365606cfc40421dc955f6cc4d8a7a46a9b2bb14
moradpoor <man22.young@gmail.com>
2023-06-29 12:42:58 +0330

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ api_id = 211111 #my.telegram.org
 api_hash = 'ff340f699c8hjsdksdkef8b34d7a039' #my.telegram.org
 token = '' #@botfather
 
-
-domain = 'example.com'
+server = '2.2.2.2' #your server ip
+domain = 'example.com' # if you don't have domain , you can set equal domain to server
 path = '/var/www/html/'
 
 bot_name = '@botname'
